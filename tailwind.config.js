6b127c58a4197f12cdb7f2a94a4e9a1abbed1850
evan70 <evanmcdan@yandex.com>
2025-04-14 22:39:57 +0200

    Aktualizovaná konfigurácia Tailwind CSS na verziu 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,60 @@
-// import defaultTheme from 'tailwindcss/defaultTheme';
-// import forms from '@tailwindcss/forms';
+// tailwind.config.js
+import tailwindcss from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//     content: [
-//         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-//         './storage/framework/views/*.php',
-//         './resources/views/**/*.blade.php',
-//     ],
-
-//     theme: {
-//         extend: {
-//             fontFamily: {
-//                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-//             },
-//         },
-//     },
-
-//     plugins: [forms],
-// };
-module.exports = {
-    content: ["./resources/**/*.blade.php"], // NOTE: changed all blades files
-    darkMode: 'media',
-    theme: {
-      screens: {
-        'xs': '375px',
-        'sm': '540px',
-        'md': '720px',
-        'lg': '960px',
-        'xl': '1140px',
-        '2xl': '1550px',
-      },
-      container: {
-        center: true,
-        padding: '20px',
-      },
+export default {
+  content: [
+    "./resources/**/*.{js,jsx,ts,tsx,vue,twig,php,html}",
+    "./src/**/*.{php,html,twig}",
+    "./templates/**/*.{html,twig}",
+  ],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      'xs': '375px',
+      'sm': '540px',
+      'md': '720px',
+      'lg': '960px',
+      'xl': '1140px',
+      '2xl': '1550px',
+    },
+    container: {
+      center: true,
+      padding: '20px',
+    },
+    fontSize: {
+      'xxs': ['14px', '1.6em'],
+      'xs': ['16px', '1.6em'],
+      'sm': ['18px', '1.6em'],
+      'md': ['20px', '1.45em'],
+      'lg': ['26px', '1.3em'],
+      'xl': ['36px', '1.3em'],
+      '2xl': ['64px', '1.1em'],
+      '3xl': ['96px', '1.1em'],
+    },
+    extend: {
       fontFamily: {
-        'sans': ['Gilroy', 'sans-serif'],
+        sans: ['Gilroy', ...defaultTheme.fontFamily.sans],
       },
-      fontSize: {
-        'xxs': ['14px', '1.6em'],
-        'xs': ['16px', '1.6em'],
-        'sm': ['18px', '1.6em'],
-        'md': ['20px', '1.45em'],
-        'lg': ['26px', '1.3em'],
-        'xl': ['36px', '1.3em'],
-        '2xl': ['64px', '1.1em'],
-        '3xl': ['96px', '1.1em'],
+      colors: {
+        white: "#FFF",
+        purple: "#7843E9",
+        pink: "#EC4176",
+        dark: "#222",
+        gray: "#454545",
+        darkblue: "#1E1F43",
+        body: '#BDBECA',
+        card: '#323359',
+        primary: {
+          DEFAULT: '#7843E9',
+          dark: '#5c2dcc',
+          light: '#9a6df0',
+          ring: 'rgba(120, 67, 233, 0.5)'
+        }
       },
-      extend: {
-        colors: {
-          white: "#FFF",
-          purple: "#7843E9",
-          pink: "#EC4176",
-          dark: "#222",
-          gray: "#454545",
-          darkblue: "#1E1F43",
-                  body: '#BDBECA',
-                  card: '#323359',
-        },
-      },
-    },
-    variants: {
-      extend: {},
-    },
-    // plugins: [
-    //       require('@tailwindcss/line-clamp'), // NOTE: this package should install by npm
-    //   ],
+      ringColor: ({ theme }) => ({
+        primary: theme('colors.primary.ring')
+      })
+    }
   }
+}
